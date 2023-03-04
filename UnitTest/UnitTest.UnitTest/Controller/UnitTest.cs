080b31c4c32983b337d9d4a08425cc57dc8ea8a7
alpercubukcu <alpercubukcuu@gmail.com>
2023-03-04 21:57:50 +0300

    Test is starter

---
+++ UnitTest/UnitTest.UnitTest/Controller/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System.Security.Cryptography.X509Certificates;
+
+namespace UnitTest.UnitTest.Controller
+{
+    [TestFixture]
+    public class UnitTest
+    {
+        [Test]
+        public void Get_All_Shop_List()
+        {
+            
+        }
+       
+    }
+}
