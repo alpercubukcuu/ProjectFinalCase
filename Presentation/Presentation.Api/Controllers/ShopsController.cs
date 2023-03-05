6d8d28a2bf1e3032456a9450e06322963224aa5b
alpercubukcu <alpercubukcuu@gmail.com>
2023-03-05 12:10:58 +0300

    Last Commit

--- Presentation/Presentation.Api/Controllers/ShopsController.cs
+++ Presentation/Presentation.Api/Controllers/ShopsController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Api.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ShopsController : ControllerBase
--- UnitTest/UnitTest.UnitTest/Controller/UnitTest.cs
+++ UnitTest/UnitTest.UnitTest/Controller/UnitTest.cs
@@ -1,12 +1,10 @@
-﻿using NUnit.Framework;
-using System.Security.Cryptography.X509Certificates;
-
+﻿
 namespace UnitTest.UnitTest.Controller
 {
-    [TestFixture]
+   
     public class UnitTest
     {
-        [Test]
+    
         public void Get_All_Shop_List()
         {
             
